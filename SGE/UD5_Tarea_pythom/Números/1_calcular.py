47fe8d8d5b5abdd542157cb58a3370a2334a0651
Alfredo Ledesma <alf.ledesma@gamil.com>
2023-04-09 21:06:45 +0200

    calculador

---
+++ SGE/UD5_Tarea_pythom/Números/1_calcular.py
@@ -0,0 +1,33 @@
+print("""Si me das dos números te enseño lo listo que puedo ser
+
+
+¿No me crees?
+
+
+pues... Intenta pillarme""")
+
+
+n1 = input("ingresa el primer número >")
+n2 = input("ingresa el segundo número >")
+
+
+n1 = int(n1)
+n2 = int(n2)
+
+
+suma = n1 + n2
+resta = n1 - n2
+multi = n1 * n2
+div = n1 / n2
+
+
+mensaje = f"""
+Para los números {n1} y {n2},
+el resultado de la suma es {suma}.
+el resultado de la resta es {resta}.
+el resultado de la multi es {multi}.
+el resultado de la división es {div}.
+"""
+
+
+print(mensaje)
